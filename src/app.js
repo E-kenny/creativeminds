c5c1c9e8ff082f17c6a38ad62b954e2a129528e7
E-kenny <ekennyobiasogu@gmail.com>
2022-08-04 18:12:43 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import List from './list';
+import Nav from './nav';
+import Footer from './footer';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {// require('@tailwindcss/aspect-ratio')
+      date: new Date()
+    }; 
+
+  }
+      
+      componentDidMount(){<Footer/>
+      this.timerID = setInterval(() => {this.tick()
+      }, 1000);
+      }
+      componentWillUnmount(){
+      clearInterval(this.timeID);
+      }
+
+      tick(){
+        this.setState({date:new Date()})
+      }
+
+      render(){
+        const state = this.state
+        return (
+          <div>
+            <Nav/>
+            <h1 className="text-3xl font-bold">Welcome!</h1>
+            <h2>{state.date.toLocaleTimeString()}.</h2>
+            <List/>
+            {/* <Footer/> */}
+          </div>
+        )
+      }
+     
+    }
+  
+  export default App
+  
