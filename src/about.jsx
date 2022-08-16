c63900d93c16502efe448b60f46b992cd7c411b5
E-kenny <ekennyobiasogu@gmail.com>
2022-08-16 19:10:08 +0100

    about page

--- src/about.jsx
+++ src/about.jsx
@@ -2,15 +2,18 @@ import Nav from './nav'
 
   export default function About() {
     return (
-        <div>
+        <div >
           <Nav/>
-          <h2 className='m-auto py-10 w-1/2 sm:w-1/3 text-2xl sm:text-5xl'>Creactive Minds</h2>
+          <div className='m-3'>
+              <h2 className='m-auto py-10 w-1/3 md:w-96 text-lg md:text-5xl'>Creactive Minds</h2>
 
-          <img className='h-screen w-full' src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="CreativeMinds" />
+              <img className='shadow h-80 w-80 sm:w-96 m-auto' src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="CreativeMinds" />
 
-          <p>
-            We are upholstery enterprise, We deal on Cabinets, Consoles, Sofas, Shelves, Wardrobes, Office tables and Beds. We Deliver both homogenized and customized products.
-          </p>
+              <p>
+                  We are upholstery enterprise, We deal on Cabinets, Consoles, Sofas, Shelves, Wardrobes, Office tables and Beds. We Deliver both homogenized and customized products.
+              </p>
+          </div>
+          
         </div>
     )
   }
